306c23effca24425a1c3119f9e5dfe57b8cb3884
hiscaler <hiscaler@gmail.com>
2018-12-20 15:33:56 +0800

    ...

--- src/response/project.go
+++ src/response/project.go
@@ -2,10 +2,10 @@ package response
 
 // 项目
 type Project struct {
-	Id string
-	Name string
-	Title string
+	Id               string
+	Name             string
+	Title            string
 	PageRenderMethod string
-	UseAgent bool
-	Props []Prop
+	UseAgent         bool
+	Props            []Prop
 }
