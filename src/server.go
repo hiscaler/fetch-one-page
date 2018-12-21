a993f1e303c2c13cac580dc2823842d3c872c6ab
hiscaler <hiscaler@gmail.com>
2018-12-21 10:08:33 +0800

    支持使用 chromdp 渲染需要爬取的页面

--- src/server.go
+++ src/server.go
@@ -8,17 +8,34 @@ import (
 	"fmt"
 	"strings"
 	"net/http"
-	"github.com/PuerkitoBio/goquery"
-	"time"
-	"net/url"
 	"config"
 	"sync"
 	"datasource"
+	"time"
+	"github.com/PuerkitoBio/goquery"
 	"strconv"
+	"net/url"
+	"github.com/chromedp/chromedp"
+	"context"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
 )
 
 var cfg config.Config
 var wg sync.WaitGroup
+
+const (
+	PageRenderNormalMethod = "normal"
+	PageRenderJsMethod     = "js"
+)
+
+const (
+	UrlPendingStatus = iota
+	UrlWorkingStatus
+	UrlFailStatus
+	UrlSuccessStatus
+	UrlCancelStatus
+)
 
 func init() {
 	cfg = *config.NewConfig()
@@ -35,155 +52,225 @@ func Read() ([]byte, bool) {
 	return content, true
 }
 
+var subRegexes map[string]string
+
+// Use chromedp open page, and get page html source
+func openPage(url string, pageSourceHtml *string, sleepSeconds time.Duration) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Sleep(sleepSeconds * time.Second),
+		chromedp.ActionFunc(func(ctx context.Context, h cdp.Executor) error {
+			node, err := dom.GetDocument().Do(ctx, h)
+			if err != nil {
+				return err
+			}
+			*pageSourceHtml, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx, h)
+			return err
+		}),
+	}
+}
 
 func FetchOne(rUrl response.Url, wg *sync.WaitGroup) {
 	wg.Done()
 	urlPath := rUrl.Url
-	log.Println("> " + urlPath)
+	log.Println(" >>> " + urlPath)
 	project := rUrl.Project
-	msg := "> Page Render method: " + project.PageRenderMethod + ", Use Agent: "
+	msg := "Page Render method: " + project.PageRenderMethod + ", Use Agent: "
 	if project.UseAgent {
 		msg += "Yes"
 	} else {
 		msg += "No"
 	}
-	resp, err := http.Get(urlPath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if resp.StatusCode == 200 {
-		respBody, err := ioutil.ReadAll(resp.Body)
+	log.Println(msg)
+	pageHtmlSource := ""
+	if project.PageRenderMethod != PageRenderNormalMethod {
+		resp, err := http.Get(urlPath)
 		if err != nil {
 			log.Fatalln(err)
-		} else {
-			// Parse page source code
-			doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(respBody)))
+		}
+		if resp.StatusCode == 200 {
+			respBody, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalln(err)
+			} else {
+				pageHtmlSource = string(respBody)
 			}
-			cellsRawData := make(map[string][]string, 0)
-			for _, prop := range project.Props {
-				propValues := []string{}
-				text := ""
-				for _, rule := range prop.Rules {
-					if len(text) > 0 {
-						break
-					}
-					if rule.RuleType == "css" {
-						// Find single item
-						//switch rule.Parser {
-						//case "text":
-						//	text = doc.Find(rule.Path).Text()
-						//case "raw":
-						//	if html, err := doc.Find(rule.Path).Html(); err == nil {
-						//		text = html
-						//	} else {
-						//		log.Println(err)
-						//	}
-						//case "attr":
-						//	text, _ = doc.Find(rule.Path).Attr(rule.Attr)
-						//}
-						// Find multiple items
-						doc.Find(rule.Path).Each(func(i int, s *goquery.Selection) {
-							switch rule.Parser {
-							case "text":
-								text = s.Text()
-							case "raw":
-								if html, err := s.Html(); err == nil {
-									text = html
-								} else {
-									log.Println(err)
-								}
-							case "attr":
-								text, _ = s.Attr(rule.Attr)
-							}
+		} else {
+			log.Fatalln("Response status code is " + string(resp.StatusCode))
+		}
+	} else if project.PageRenderMethod == PageRenderNormalMethod {
+		var err error
+		// create context
+		ctxt, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-							propValues = append(propValues, text)
-						})
-					} else if rule.RuleType == "xpath" {
-						// todo
-						log.Println("Parse type is XPath, Don't implemented.")
-					}
-				}
-				cellsRawData[prop.Name] = propValues
-			}
-			log.Println(fmt.Sprintf("%#v", cellsRawData))
+		// create chrome instance
+		c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			cells := make([]map[string]string, 0)
-			if len(cellsRawData) > 0 {
-				usedPropName := ""
-				minSize := 1024
-				for name, items := range cellsRawData {
-					l := len(items)
-					if l != 0 && l < minSize {
-						minSize = l
-						usedPropName = name
-					} else if minSize == 1 {
-						break
-					}
-				}
+		// run task list
+		err = c.Run(ctxt, openPage(urlPath, &pageHtmlSource, 20))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-				for k, v := range cellsRawData[usedPropName] {
-					cellValue := map[string]string{
-						usedPropName: v,
-					}
-					for _, prop := range project.Props {
-						if prop.Name != usedPropName {
-							cellValue[prop.Name] = cellsRawData[prop.Name][k]
-						}
-					}
-					log.Println(fmt.Sprintf("%#v", cellValue))
-					cells = append(cells, cellValue)
-				}
-			}
-			log.Println(fmt.Sprintf("%#v", cells))
+		// shutdown chrome
+		err = c.Shutdown(ctxt)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			for i, cell := range cells {
-				log.Println("Callback #" + strconv.Itoa(i) + " Data")
-				params := url.Values{}
-				params.Add("url_id", rUrl.Id)
-				cellJson, _ := json.Marshal(cell)
-				params.Add("cell", string(cellJson))
-				if cfg.Debug {
-					log.Println(fmt.Sprintf("%+v", params))
-				}
-				// Send to document api
-				payload := strings.NewReader(params.Encode())
-				client := &http.Client{
-					Timeout: time.Second * 5,
-				}
-				req, err := http.NewRequest("POST", cfg.ApiEndpoint+"/document?url_id="+rUrl.Id, payload)
-				if err != nil {
-					log.Fatalln("Request error: " + err.Error())
-				}
-				req.Body.Close()
-				req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-				resp, err := client.Do(req)
-				if err != nil {
-					log.Fatalln("Response error: ", err)
-				}
-				if resp.StatusCode != 404 {
-					respBody, _ := ioutil.ReadAll(resp.Body)
-					if resp.StatusCode == 200 {
-						log.Println("Callback is successful")
-					} else {
-						log.Println("Callback is failed, http code is " + resp.Status)
-					}
-					if cfg.Debug {
-						log.Println("Callback return message: " + string(respBody))
-					}
-				} else {
-					log.Println("Not found callback api" + req.URL.String())
-				}
-				resp.Body.Close()
-			}
-
+		// wait for chrome to finish
+		err = c.Wait()
+		if err != nil {
+			log.Fatal(err)
 		}
 	} else {
-		log.Fatalln("Response status code is " + string(resp.StatusCode))
+		log.Println("Unkonw page render method: " + project.PageRenderMethod)
 	}
-	log.Println(msg)
+
+	if len(pageHtmlSource) > 0 {
+		// Parse page source code
+		fmt.Println("pageHtmlSource = ", string(pageHtmlSource))
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(pageHtmlSource))
+		if err != nil {
+			log.Fatal(err)
+		}
+		cellsRawData := make(map[string][]string, 0)
+		for _, prop := range project.Props {
+			propValues := []string{}
+			text := ""
+			for _, rule := range prop.Rules {
+				if len(text) > 0 {
+					break
+				}
+				if rule.RuleType == "css" {
+					// Find single item
+					//switch rule.Parser {
+					//case "text":
+					//	text = doc.Find(rule.Path).Text()
+					//case "raw":
+					//	if html, err := doc.Find(rule.Path).Html(); err == nil {
+					//		text = html
+					//	} else {
+					//		log.Println(err)
+					//	}
+					//case "attr":
+					//	text, _ = doc.Find(rule.Path).Attr(rule.Attr)
+					//}
+					// Find multiple items
+					doc.Find(rule.Path).Each(func(i int, s *goquery.Selection) {
+						switch rule.Parser {
+						case "text":
+							text = s.Text()
+						case "raw":
+							if html, err := s.Html(); err == nil {
+								text = html
+							} else {
+								log.Println(err)
+							}
+						case "attr":
+							text, _ = s.Attr(rule.Attr)
+						}
+
+						propValues = append(propValues, text)
+					})
+				} else if rule.RuleType == "xpath" {
+					// todo
+					log.Println("Parse type is XPath, Don't implemented.")
+				}
+			}
+			cellsRawData[prop.Name] = propValues
+		}
+		log.Println(fmt.Sprintf("%#v", cellsRawData))
+
+		cells := make([]map[string]string, 0)
+		if len(cellsRawData) > 0 {
+			usedPropName := ""
+			minSize := 1024
+			for name, items := range cellsRawData {
+				l := len(items)
+				if l != 0 && l < minSize {
+					minSize = l
+					usedPropName = name
+				} else if minSize == 1 {
+					break
+				}
+			}
+
+			for k, v := range cellsRawData[usedPropName] {
+				cellValue := map[string]string{
+					usedPropName: v,
+				}
+				for _, prop := range project.Props {
+					if prop.Name != usedPropName {
+						cellValue[prop.Name] = cellsRawData[prop.Name][k]
+					}
+				}
+				log.Println(fmt.Sprintf("%#v", cellValue))
+				cells = append(cells, cellValue)
+			}
+		}
+		log.Println(fmt.Sprintf("%#v", cells))
+
+		for i, cell := range cells {
+			log.Println("Callback #" + strconv.Itoa(i) + " Data")
+			params := url.Values{}
+			params.Add("url_id", rUrl.Id)
+			cellJson, _ := json.Marshal(cell)
+			params.Add("cell", string(cellJson))
+			if cfg.Debug {
+				log.Println(fmt.Sprintf("%+v", params))
+			}
+			// Send to document api
+			payload := strings.NewReader(params.Encode())
+			client := &http.Client{
+				Timeout: time.Second * 5,
+			}
+			req, err := http.NewRequest("POST", cfg.ApiEndpoint+"/document?url_id="+rUrl.Id, payload)
+			if err != nil {
+				log.Fatalln("Request error: " + err.Error())
+			}
+			req.Body.Close()
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatalln("Response error: ", err)
+			}
+			if resp.StatusCode != 404 {
+				respBody, _ := ioutil.ReadAll(resp.Body)
+				if resp.StatusCode == 200 {
+					log.Println("Save document is successful")
+					rUrl.Status = UrlSuccessStatus
+				} else {
+					log.Println("Save document is failed, http code is " + resp.Status)
+					rUrl.Status = UrlFailStatus
+					rUrl.CallbackMessage = "Save document is failed, http code is " + resp.Status
+				}
+				if err := rUrl.Callback(cfg.ApiEndpoint); err != nil {
+					log.Println("URL callback error" + err.Error())
+				}
+				if cfg.Debug {
+					log.Println("Save document return message: " + string(respBody))
+				}
+			} else {
+				log.Println("Not found save document api" + req.URL.String())
+			}
+			resp.Body.Close()
+		}
+	} else {
+		log.Println("Can't get page HTML source from " + urlPath)
+		// Url 内容获取失败回调
+		rUrl.Status = UrlFailStatus
+		rUrl.CallbackMessage = "Can't get page html source."
+		if err := rUrl.Callback(cfg.ApiEndpoint); err != nil {
+			log.Println("URL callback error" + err.Error())
+		}
+	}
 }
+
 func main() {
 	jsonByte := []byte{}
 	ok := false
